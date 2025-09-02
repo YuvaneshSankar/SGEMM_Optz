2cbc21fbb8654332284048b557879122d849e201
YuvaneshSankar <yuvanesh.skv@gmail.com>
2025-09-02 16:36:36 +0530

    SMEM Tiling

--- 03_SMEM_Tiling.cu
+++ 03_SMEM_Tiling.cu
@@ -10,6 +10,58 @@
 #define CUDA_CHECK(err) {if (err != cudaSuccess){printf("%s in %s at line %d \n", cudaGetErrorString(err), __FILE__, __LINE__);exit(EXIT_FAILURE);}}
 
 
+
+__global__ void tiled_mat_mul_kernel(float *A, float *B, float *C, int m, int n, int p){
+
+    // Ensure that TILE_WIDTH = BLOCK_SIZE
+    assert(TILE_WIDTH == blockDim.x);
+    assert(TILE_WIDTH == blockDim.y);
+
+    int by=blockIdx.y;
+    int bx=blockIdx.x;
+
+    int ty=threadIdx.y;
+    int tx=threadIdx.x;
+
+    int i=by*TILE_WIDTH + ty;
+    int j=bx*TILE_WIDTH + tx;
+
+    __shared__ float sh_A[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float sh_B[TILE_WIDTH][TILE_WIDTH];
+
+    float val=0.0f;
+
+    for(int tileId=0;tileId<ceil((float)n/TILE_WIDTH);tileId++){
+
+        if(i<m && (tileId*TILE_WIDTH + tx)<n){
+            sh_A[ty][tx]=A[i*n + tileId*TILE_WIDTH + tx];
+        }
+        else{
+            sh_A[ty][tx]=0.0f;
+        }
+        if(j<p && (tileId*TILE_WIDTH + ty)<n){
+            sh_B[ty][tx]=B[(tileId*TILE_WIDTH + ty)*p + j];
+        }
+        else{
+            sh_B[ty][tx]=0.0f;
+        }
+        __syncthreads();
+
+
+        for(int k=0;k<TILE_WIDTH;k++){
+            val+=sh_A[ty][k]*sh_B[k][tx];
+        }
+        __syncthreads();
+
+    }
+
+    if(i<m && j<p){
+        C[i*p + j]=val;
+    }
+}
+
+
+
 void init_matrix(float *mat , int row , int col){
     for(int i=0;i<row * col ;i++){
         mat[i]=(float)rand()/RAND_MAX;
@@ -49,9 +101,7 @@ int main()
     dim3 dim_grid(ceil(P/(float)(TILE_WIDTH)), ceil(M/(float)(TILE_WIDTH)), 1);
     tiled_mat_mul_kernel<<<dim_grid, dim_block>>>(d_A, d_B, d_C, M, N, P);
 
-    // Copy back results
-    cudaError_t err_C_ = cudaMemcpy(C, d_C, M*P*sizeof(float), cudaMemcpyDeviceToHost);
-    CUDA_CHECK(err_C_);
+    cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost);
 
     // Free memory
     cudaFree(d_A);
