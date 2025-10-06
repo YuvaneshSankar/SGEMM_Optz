14b287e03eee40d57362448bb127fc59a3b1641f
YuvaneshSankar <yuvanesh.skv@gmail.com>
2025-10-07 03:55:54 +0530

    feat(06_):Implement CUDA matrix multiplication with thread coarsening factor

---
+++ 06_COARSENING_FACTOR.cu
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <cuda_runtime.h>
+#include <time.h>
+
+
+
+#define M 1024 
+#define N 512 
+#define P 2048  
+#define BLOCK_SIZE 32
+#define COARSENING_FACTOR 4
+
+__global__ void kernel(float * A , float * B , float * C , int m , int n , int p){
+    int col= blockDim.x*blockIdx.x + threadIdx.x;
+    int base_row= (blockDim.y*blockIdx.y + threadIdx.y )* COARSENING_FACTOR;
+    for(int i=0;i<COARSENING_FACTOR;i++){
+        int row=base_row+i;
+        if(row<m && col<p){
+            float sum=0.0f;
+            for(int k=0;k<n;k++){
+                sum+=A[row*n+k]*B[k*p+col];
+            }
+            C[row*p+col]=sum;
+        }
+    }
+}
+
+void init_matrix(float *mat , int row , int col){
+    for(int i=0;i<row * col ;i++){
+        mat[i]=(float)rand()/RAND_MAX;
+    }
+}
+
+
+
+int main(){
+    float *h_A,*h_B,*h_C;
+    float *d_A,*d_B,*d_C;
+
+    int size_A=M*N*sizeof(float);
+    int size_B=N*P*sizeof(float);
+    int size_C=M*P*sizeof(float);
+
+    //allocate host memeory
+    h_A=(float*)malloc(size_A);
+    h_B=(float*)malloc(size_B);
+    h_C=(float*)malloc(size_C);
+
+    //intialize matrices
+    init_matrix(h_A,M,N);
+    init_matrix(h_B,N,P);
+
+    //allocate device memeory
+    cudaMalloc(&d_A,size_A);
+    cudaMalloc(&d_B,size_B);
+    cudaMalloc(&d_C,size_C);
+
+    //copt data from host to device
+    cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
+
+    //launch kernel
+    dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 gridDim((P + BLOCK_SIZE - 1) / BLOCK_SIZE, (M + BLOCK_SIZE - 1) / BLOCK_SIZE);
+
+    //warm up runs
+
+    for(int i=0;i<3;i++){
+        kernel<<<gridDim,blockDim>>>(d_A,d_B,d_C,M,N,P);
+        cudaDeviceSynchronize();
+    }
+
+
+    //Benchmark implementation
+    double gpu_total_time=0.0;
+    for(int i=0;i<20;i++){
+        kernel<<<gridDim,blockDim>>>(d_A,d_B,d_C,M,N,P);
+        cudaDeviceSynchronize();
+    }
+
+    cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost);
+
+    // Free memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    return 0;   
+    
+}
