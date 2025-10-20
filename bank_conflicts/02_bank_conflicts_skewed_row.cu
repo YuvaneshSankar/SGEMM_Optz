6edf72d95309c783f80bf4d0c979d7505a375cca
Yuvanesh Sankar <yuvanesh.skv@gmail.com>
2025-10-20 10:05:40 +0530

    refractor(01_bank_conflicts_padding.cu) : updated bank conflicts solved using padding

---
+++ bank_conflicts/02_bank_conflicts_skewed_row.cu
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <cuda_runtime>
+
+
+#define block_size 16
+#define N 32
+
+
+__global__ void skewedRowTranspose(float *out, float *in,int width){
+  
+}
+
+int main(){
+
+}
